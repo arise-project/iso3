461494febd080fa86847f62df4fa4859c5823c0d
arise-project <pirogov.e@gmail.com>
2019-02-26 01:20:31 +0200

    iterative

--- samples.cs
+++ samples.cs
@@ -31,3 +31,37 @@ public class Solution {
     }
 }
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    public IList<int> PreorderTraversal(TreeNode root) {
+        List<int> r = new List<int>();
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        s.Push(root);
+        while(s.Count > 0)
+        {
+            TreeNode n = s.Pop();
+            
+            r.Add(n.val);
+            
+            if(n.left != null)
+            {
+                s.Push(n.left);
+            }
+            
+            if(n.right != null)
+            {
+                s.Push(n.right);
+            }
+        }
+        return r;
+    }
+}
