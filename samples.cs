89a81223924f72490978cd775101d6df4978f6e4
arise-project <pirogov.e@gmail.com>
2019-02-27 23:47:45 +0200

    two sum

--- samples.cs
+++ samples.cs
@@ -65,3 +65,65 @@ public class Solution {
         return r;
     }
 }
+
+
+1. Two Sum
+Easy
+
+9876
+
+320
+
+Favorite
+
+Share
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+    
+  public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, List<int>> ind = new Dictionary<int, List<int>>();
+        for(int i =0; i < nums.Length; i++)
+        {
+            if(nums[i] < target)
+            {
+                int sub = (int)Math.Abs(nums[i] - target);
+                if(ind.ContainsKey(nums[i]))
+                { 
+                    ind[nums[i]].Add(i);
+                }
+                else
+                {
+                    ind[nums[i]] = new List<int>{ i };
+                }
+                   
+                if(ind.ContainsKey(sub))
+                { 
+                    ind[sub].Add(i);
+                }
+                else
+                {
+                    ind[sub] = new List<int>{ i };
+                }
+            }            
+        }
+                   
+        foreach(int num in ind.Keys)
+        {
+            if(ind[num].Count > 1)
+            {
+                return ind[num].ToArray();
+            }
+        }
+        
+        return null;
+    }
+}
