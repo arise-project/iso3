d2e1811ba229a331a45ef0d35486dda7c8322aba
arise-project <pirogov.e@gmail.com>
2019-02-28 23:54:27 +0200

    life

--- samples.cs
+++ samples.cs
@@ -120,3 +120,37 @@ return [0, 1].
         return null;
     }
 }
+
+
+public class Solution
+{
+    public int[] cellCompete(int[] states, int days)
+    {
+     int[] newStates = new int[states.Length];
+     for(int i = 0; i < days; i++)
+     {
+         for(int j = 0; j < states.Length; j++)
+         {
+             int left = 0;
+             int right = 0;
+     
+             if(j >0)
+             {
+                 left = states[j-1];
+             }
+             
+             if(j <states.Length -1)
+             {
+                 right = states[j+1];
+             }
+             
+             newStates[j] = left ^ right;
+         }
+         
+        System.Array.Copy(newStates, 0, states, 0, states.Length);
+     }
+     
+     return states;
+    }
+    
+}
